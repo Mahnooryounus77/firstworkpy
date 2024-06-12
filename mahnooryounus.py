ffce1f18ce6fe0aafa3f1fadb8015e45fd0d0456
usermano77 <manohere772@gmail.com>
2024-06-12 09:05:50 +0500

    Add files via upload

---
+++ mahnooryounus.py
@@ -0,0 +1,14 @@
+#FOR AREA OF TRIANGLE
+Base=4
+Height=6.3
+area_of_triangle=0.5*Base*Height
+print("The Area of triangle is:",area_of_triangle)
+#FOR AREA OF A SQUARE
+Side=12
+Area_of_Square=Side*Side
+print("Area of square is:",Area_of_Square)
+#FOR AREA OF CIRCLE
+Radius=7
+pi=3.142
+Area_of_Circle =pi*Radius*Radius
+print("Area of circle:",Area_of_Circle)
